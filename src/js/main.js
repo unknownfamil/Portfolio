93d0335c054842f54e46423a6d43daec44934101
unknownfamil <maksutismailov12345678@gmail.com>
2023-12-05 19:19:05 +0600

    Updated files

--- src/js/main.js
+++ src/js/main.js
@@ -17,36 +17,8 @@ menuLinks.forEach((el) => {
     });
 });
 
-// const form = document.getElementById('commentForm');
-// form.addEventListener('submit', (event) => {
-//     event.preventDefault();
-//     const name = document.getElementById('name');
-//     const email = document.getElementById('email');
-//     const comment = document.getElementById('comment');
 
-//     const data = {
-//         name,
-//         email,
-//         comment
-//     };
 
-//     fetch('Url', {
-//         method: 'POST',
-//         headers:  {
-//             'content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(data)
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         if (data.success) {
-//             alert('Your comment has been submitted succesfully!');
-//             form.reset();
-//         } else {
-//             alert('There was an error submiting your comment.');
-//         }
-//     });
-// });
 
 
 
